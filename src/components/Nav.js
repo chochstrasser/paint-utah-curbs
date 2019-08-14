3366dc73c2089ee5bbbaab9b5629d67d8ec563c7
Chase Hochstrasser <chochstrasser@sofi.org>
2019-08-13 19:21:49 -0600

    First commit

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <>
+      <h1>Paint Utah Curbs</h1>
+    </>
+  );
+};
+
+export default Nav;
---
+++ src/components/contact-form.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ContactForm = ({ handleOnClick }) => {
+  return (
+    <form id="contactForm">
+      <label>Name</label>
+      <input type="text" placeholder="Name" name="name" required />
+      <label>Email Address</label>
+      <input type="email" placeholder="Email Address" name="email" required />
+      <label>Message</label>
+      <textarea rows="5" placeholder="Message" name="content" required />
+      <div id="toast" />
+      <button type="submit" id="submit" onclick={handleOnClick} disabled>
+        Send
+      </button>
+    </form>
+  );
+};
+export default ContactForm;
