fc8d44632f1b7d3a862e3f503a4ee70282197453
Michal Myczkowski <mmyczkowski@mac.com>
2019-08-18 23:53:40 +0200

    add global font

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from "react";
 import MainNav from "./components/nav/main-nav";
 import ContactForm from "./components/contact-form/contact-form";
 import Header from "./components/header/header";
+import Welcome from "./components/welome/welcome";
+import styled from "styled-components";
 
+const MainStyle = styled.div`
+  font-family: Lato, Helvetica, Arial, Helvetica, sans-serif;
+`;
 const App = () => {
   return (
-    <>
+    <MainStyle>
       <MainNav />
       <Header />
+      <Welcome />
       <ContactForm />
-    </>
+    </MainStyle>
   );
 };
 
