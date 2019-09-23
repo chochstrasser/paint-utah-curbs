ac37d9fbd2a32ba83886c97d9dcf8f7eb5d243c8
Chase Hochstrasser <chochstrasser@sofi.org>
2019-09-23 09:33:36 -0600

    clean up

--- src/components/nav-bar/nav-bar-styles.js
+++ src/components/nav-bar/nav-bar-styles.js
@@ -5,7 +5,7 @@ export const Navigation = styled.div`
   top: 0;
   left: 0;
   width: 100%;
-  height: ${props => props.collapse ? "100vh" : "90px"};
+  height: ${props => (props.collapse ? "100vh" : "90px")};
   background-color: whitesmoke;
   border-bottom: 0.1px solid grey;
 `;
@@ -21,9 +21,7 @@ export const Container = styled.div`
   margin: 0 auto;
   @media only screen and (max-device-width: 1024px) {
     flex-flow: column-wrap;
-  };
- 
-  
+  }
 `;
 
 export const StyledBold = styled.b`
@@ -32,20 +30,17 @@ export const StyledBold = styled.b`
 `;
 
 export const Logo = styled.div`
-order: 1;
+  order: 1;
   font-size: 30px;
   font-weight: 300;
   margin: 0 auto;
   padding: 1.5rem 0;
   @media only screen and (max-device-width: 1024px) {
     flex-basis: calc(90vw - 20px);
-  };
-  
-
+  }
 `;
 
 export const StyledNav = styled.nav`
-
   display: flex;
   order: 2;
   margin-right: 30px;
@@ -55,11 +50,9 @@ export const StyledNav = styled.nav`
 
   @media only screen and (max-device-width: 1024px) {
     order: 3;
-    display: ${props => props.collapse ? "block" : "none"};
+    display: ${props => (props.collapse ? "block" : "none")};
     align-self: flex-end;
-
-  };
-
+  }
 `;
 
 export const StyledBurger = styled.div`
@@ -68,18 +61,13 @@ export const StyledBurger = styled.div`
   align-self: center;
   @media only screen and (min-device-width: 1025px) {
     display: none;
-  };
+  }
   @media only screen and (max-device-width: 1024px) {
     order: 2;
-  };
-
+  }
 `;
 
-
-export const StyledUl = styled.ul`
-
- 
-  `;
+export const StyledUl = styled.ul``;
 
 export const StyledLi = styled.li`
   list-style-type: none;
@@ -90,15 +78,13 @@ export const StyledLi = styled.li`
   text-transform: uppercase;
 
   @media only screen and (max-device-width: 1024px) {
-    display: ${props => props.collapse ? "none" : "block"};
+    display: ${props => (props.collapse ? "none" : "block")};
     width: 100%;
     text-align: center;
     padding: 10px 0px;
     font-size: 1.3rem;
     text-decoration: none;
-
-  };
-
+  }
 
   :active {
     background-color: black;
